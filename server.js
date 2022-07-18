32e7234e963837bcf560e485933c28f220ca180b
jykyle0226 <jykyle0226@gmail.com>
2022-07-18 10:46:24 -0400

    back end

--- server.js
+++ server.js
@@ -42,19 +42,6 @@ app.use(express.urlencoded({ extended: true }))
 // app.use(locationsController);
 
 
-app.post('/signup', async (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password
-  }
-  const userResponse = await admin.auth().createUser({
-    email: user.email,
-    password: user.password,
-    emailVerified: false,
-    disabled: false
-  })
-  res.json(userResponse)
-})
 
 // Route
 app.get("/", (req, res) => {
